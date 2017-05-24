b27729e8294f91604e0b832f349706a239e5af1e
berghout <d.l.berghout@dictu.nl>
2017-05-24 15:11:47 +0200

    insert script for table sms_applicatie_register.

---
+++ sms_applicatie_regsiter.sql
@@ -0,0 +1,75 @@
+SET DEFINE OFF;
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE)
+ Values
+   ('SMS', 'PW_ANONYMUS', 'SMS_RAADPLEGER123');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE, OMSCHRIJVING)
+ Values
+   ('SMS', 'ALLEEN_BEHEERDER', 'N', 'N=nee (iedereen toegang)
+J=ja (uitsluitend beheerder heeft toegang tot ISMS) ');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE)
+ Values
+   ('SMS', 'REGELGEVING21', 'ISO-27001A');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE)
+ Values
+   ('SMS', 'REGELGEVING22', 'ISO-27001');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE)
+ Values
+   ('SMS', 'REGELGEVING23', 'BIR');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE, OMSCHRIJVING)
+ Values
+   ('SMS', 'REGELGEVING3', 'Landbouwwet', 'De landbouwwet wordt voorzien van artikelen uit het BIR ');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE, OMSCHRIJVING)
+ Values
+   ('SMS', 'REGELGEVING8', 'Test3', 'test3');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE)
+ Values
+   ('SMS', 'IB_ORGANISATIE', 'Deze tekst is te onderhouden in het register. Variabele: IB_ORGANISATIE. (home>beheer>register) ');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE, OMSCHRIJVING)
+ Values
+   ('SMS', 'REGELGEVING 16', 'haha', 'hi hi');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE)
+ Values
+   ('SMS', 'REGELGEVING12', 'ISOTEST');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE, OMSCHRIJVING)
+ Values
+   ('SMS', 'REGELGEVING1', 'ISO-9001', 'Te gebruiken in hoofdstuk');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE, OMSCHRIJVING)
+ Values
+   ('SMS', 'REGELGEVING4', 'WBP', 'Wet Beschermin Persoonsgegevens');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE)
+ Values
+   ('SMS', 'REGELGEVING5', 'ISO-27001FEIKE');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE, OMSCHRIJVING)
+ Values
+   ('SMS', 'REGELGEVING2', 'ISO-27001-A', 'Te gebruiken in hoofdstuk');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE)
+ Values
+   ('SMS', 'SECURITY_INCIDENT', 'Deze tekst is te onderhouden in het register. Variabele: SECURITY_INCIDENT. (home>beheer>register) ');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE)
+ Values
+   ('SMS', 'WELKOMTEKST', 'Deze tekst is te onderhouden in het register. Variabele: WELKOMTEKST. (home>beheer>register) ');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE, OMSCHRIJVING)
+ Values
+   ('SMS', 'REGELGEVING9', 'TestImport', 'Regelgeving TestImport 2015');
+Insert into SMS_APPLICATIE_REGISTER
+   (APPLICATIE, VARIABELE, WAARDE, OMSCHRIJVING)
+ Values
+   ('SMS', 'REGELGEVING25', 'groot', 'grootst');
+COMMIT;
