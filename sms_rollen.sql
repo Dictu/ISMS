6f5a7ddd5bfb6fe37a16218e64ca1871dc847e0e
berghout <d.l.berghout@dictu.nl>
2017-05-24 14:56:35 +0200

    insert script for table sms_rollen

---
+++ sms_rollen.sql
@@ -0,0 +1,346 @@
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM)
+ Values
+   (1501, 'SMS_OWNER', 'ONDERHOUD');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1482, 'SMS_OWNER', 'Onderhoud OBW', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1661, 'SMS_OWNER', 'Rapporten', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1662, 'SMS_OWNER', 'MT Rapporten', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1682, 'SMS_OWNER', 'Overzicht Risico analyse', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1721, 'SMS_OWNER', 'BEHEERDER', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1823, 'SMS_OWNER', 'Foutenlog', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1821, 'SMS_OWNER', 'Foutboodschappen', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1921, 'SMS_OWNER', 'ACCEPTANT', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, OMSCHRIJVING, SCHERM_IND)
+ Values
+   (1881, 'SMS_OWNER', 'Test3', 'Test3rol', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1521, 'SMS_OWNER', 'Overzicht OBW', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1523, 'SMS_OWNER', 'Onderhoud RACI', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1524, 'SMS_OWNER', 'Overzicht RACI', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1525, 'SMS_OWNER', 'Overzicht ACTIES', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1526, 'SMS_OWNER', 'Onderhoud ACTIES', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1527, 'SMS_OWNER', 'Overzicht Regelgeving', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1528, 'SMS_OWNER', 'Onderhoud Regelgeving', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1529, 'SMS_OWNER', 'Overzicht OBW wijzigingen', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1530, 'SMS_OWNER', 'Goedkeuren OBW wijzigingen', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1531, 'SMS_OWNER', 'Overzicht Best Practices', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1532, 'SMS_OWNER', 'Onderhoud Best Practices', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1533, 'SMS_OWNER', 'Onderhoud Documenten', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1534, 'SMS_OWNER', 'Overzicht Documenten', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1535, 'SMS_OWNER', 'Overzicht Document wijzigingen', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1536, 'SMS_OWNER', 'Overzicht Documentsoorten', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1537, 'SMS_OWNER', 'Onderhoud Documentsoorten', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1538, 'SMS_OWNER', 'Onderhoud Gebruikers', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1539, 'SMS_OWNER', 'Onderhoud Batches', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1540, 'SMS_OWNER', 'Onderhoud Log', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1541, 'SMS_OWNER', 'Onderhoud Register', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1542, 'SMS_OWNER', 'Onderhoud Processen', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1543, 'SMS_OWNER', 'Onderhoud Toetsen', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1544, 'SMS_OWNER', 'Onderhoud Projecten', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1545, 'SMS_OWNER', 'Onderhoud Organisaties', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1546, 'SMS_OWNER', 'Onderhoud Verantwoording', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1547, 'SMS_OWNER', 'Onderhoud Rollen', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1561, 'SMS_OWNER', 'Goedkeuren Doc wijzigingen', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1681, 'SMS_OWNER', 'Onderhoud Risico analyse', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1683, 'SMS_OWNER', 'Onderhoud Risk Treatment Plan', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1684, 'SMS_OWNER', 'Overzicht Risk Treatment Plan', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1685, 'SMS_OWNER', 'Onderhoud RTP - Norm & Risico', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1686, 'SMS_OWNER', 'Overzicht RTP - Norm & Risico', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, OMSCHRIJVING, SCHERM_IND)
+ Values
+   (1901, 'SMS_OWNER', 'Krullenjongen', 'asfasdfasdf', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, OMSCHRIJVING, SCHERM_IND)
+ Values
+   (2361, 'SMS_OWNER', 'Laden nieuwe regelgeving', 'Import nieuwe regelgeving en nieuwe regelgeving', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1979, 'SMS_OWNER', 'TL IN STAND HOUDEN', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1980, 'SMS_OWNER', 'TL INVOEREN', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1981, 'SMS_OWNER', 'COORDINATOR IB BEDRIJFSVOERING', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1982, 'SMS_OWNER', 'AUTORISATIE OFFICER', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1983, 'SMS_OWNER', 'DIRECTIE DICTU', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1984, 'SMS_OWNER', 'MT BEDRIJFSVOERING', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1985, 'SMS_OWNER', 'MT DICTU', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1986, 'SMS_OWNER', 'SECURITY OFFICER', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1987, 'SMS_OWNER', 'TL OPERATING SYSTEMS', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1988, 'SMS_OWNER', 'TL A& S', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1989, 'SMS_OWNER', 'TL BEDRIJFSADMINISTRATIE', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1990, 'SMS_OWNER', 'TL C& O', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1991, 'SMS_OWNER', 'TL CONTRACTMANAGEMENT', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1992, 'SMS_OWNER', 'TL DATACOMMUNICATIE', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1993, 'SMS_OWNER', 'TL DD& R', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1994, 'SMS_OWNER', 'TL DICTU-DESK', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1995, 'SMS_OWNER', 'TL H& T', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1996, 'SMS_OWNER', 'TL KETENMANAGEMENT', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1997, 'SMS_OWNER', 'TL KWALITEITSBORGING', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1998, 'SMS_OWNER', 'TL MIDDLEWARE', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (1999, 'SMS_OWNER', 'TL SUPPORT UITVOERING', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2000, 'SMS_OWNER', 'TL SUBSIDIEVERSTREKKINGEN', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2001, 'SMS_OWNER', 'MT UITVOERING INFRA', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2002, 'SMS_OWNER', 'MT UITVOERING AB', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2003, 'SMS_OWNER', 'MT STURING', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2004, 'SMS_OWNER', 'TL RELATIEMANAGEMENT
+', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2005, 'SMS_OWNER', 'COORDINATOR IB UITVOERING', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2006, 'SMS_OWNER', 'COORDINATOR IB STURING', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2007, 'SMS_OWNER', 'IB SPECIALIST UITVOERING AB', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2008, 'SMS_OWNER', 'IB SPECIALIST UITVOERING', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2009, 'SMS_OWNER', 'TL DELIVERY MANAGEMENT', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2010, 'SMS_OWNER', 'TL ONDERSTEUNEN', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2011, 'SMS_OWNER', 'COORDINATOR UB', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2012, 'SMS_OWNER', 'TL KERNDEPARTEMENT', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2013, 'SMS_OWNER', 'COORDINATOR IB DC', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2014, 'SMS_OWNER', 'RISICOMANAGER', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2015, 'SMS_OWNER', 'AUDITOR', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (2016, 'SMS_OWNER', 'MT DIENSTENCENTRUM', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (3081, 'SMS_OWNER', 'Nieuwe regelgeving', 'J');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, OMSCHRIJVING, SCHERM_IND)
+ Values
+   (4021, 'SMS_OWNER', 'test', 'test', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (3481, 'SMS_OWNER', 'FEIKE', 'N');
+Insert into APX_ROLLEN
+   (ID, "SCHEMA", NAAM, SCHERM_IND)
+ Values
+   (3501, 'SMS_OWNER', 'PE Service Operations', 'N');
+COMMIT;
