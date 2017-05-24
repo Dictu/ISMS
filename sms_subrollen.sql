e3dd1d3476d066158a2ac962f61a18afea6db097
berghout <d.l.berghout@dictu.nl>
2017-05-24 15:00:11 +0200

    insert script for table apx_subrollen

---
+++ sms_subrollen.sql
@@ -0,0 +1,338 @@
+SET DEFINE OFF;
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2142, 1721, 1530);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2021, 1501, 1482);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2143, 1721, 1662);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2144, 1721, 1501);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2145, 1721, 1526);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2146, 1721, 1539);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2147, 1721, 1532);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2148, 1721, 1533);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2149, 1721, 1537);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2150, 1721, 1538);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2151, 1721, 1540);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2152, 1721, 1482);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2153, 1721, 1545);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2154, 1721, 1542);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2155, 1721, 1544);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2156, 1721, 1523);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2157, 1721, 1685);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2158, 1721, 1528);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2159, 1721, 1541);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2160, 1721, 1681);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2161, 1721, 1683);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2162, 1721, 1547);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2141, 1721, 1561);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2163, 1721, 1543);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2164, 1721, 1546);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2165, 1721, 1525);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2166, 1721, 1531);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2167, 1721, 1535);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2168, 1721, 1534);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2169, 1721, 1536);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2170, 1721, 1521);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2171, 1721, 1529);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2172, 1721, 1524);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2173, 1721, 1686);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2174, 1721, 1527);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2175, 1721, 1682);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2176, 1721, 1684);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2177, 1721, 1661);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2301, 1921, 1535);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2302, 1921, 1529);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2321, 1721, 1823);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2281, 1721, 1821);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2341, 1901, 2015);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (2381, 1501, 1921);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (3201, 1721, 2015);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4061, 1721, 3081);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4062, 1721, 2361);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4261, 3501, 1526);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4262, 3501, 1527);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4263, 3501, 1533);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4264, 3501, 1534);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4265, 3501, 1661);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4266, 3501, 1662);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4267, 3501, 1482);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4268, 3501, 1525);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4269, 3501, 1521);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4281, 1987, 1526);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4282, 1987, 1533);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4283, 1987, 1482);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4284, 1987, 1525);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4285, 1987, 1534);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4286, 1987, 1521);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4287, 1987, 1661);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4288, 1987, 1527);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4289, 1987, 1662);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4301, 2010, 1526);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4302, 2010, 1533);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4303, 2010, 1482);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4304, 2010, 1525);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4305, 2010, 1534);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4306, 2010, 1521);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4307, 2010, 1661);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4308, 2010, 1527);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4309, 2010, 1662);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4321, 2006, 1526);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4322, 2006, 1533);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4323, 2006, 1662);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4324, 2006, 1482);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4325, 2006, 1534);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4326, 2006, 1521);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4327, 2006, 1527);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4328, 2006, 1525);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4329, 2006, 1661);
+Insert into APX_SUBROLLEN
+   (ID, HOOFDROL_ID, SUBROL_ID)
+ Values
+   (4821, 4021, 1533);
+COMMIT;
