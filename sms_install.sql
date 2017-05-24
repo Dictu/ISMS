7038e8267b0c63102feee05abdb4c95028646787
berghout <d.l.berghout@dictu.nl>
2017-05-24 15:18:47 +0200

    script to install the application
    script to install the application in an Oracle database.
    script calls the other scripts.

---
+++ sms_install.sql
@@ -0,0 +1,10 @@
+-- Install the application.
+
+@@sms_sequence.sql
+@@sms_tabellen.sql
+@@sms_views.sql
+@@sms_packages.sql
+@@sms_rollen.sql
+@@sms_subrollen.sql
+@@sms_raci.sql
+@@sms_applicatie_register.sql
